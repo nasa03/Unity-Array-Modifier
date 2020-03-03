2e8856eb221f991f773f8c7e2ee4d0f7b61a3511
Samuel <samuel.karabetian@gmail.com>
2020-03-03 16:12:31 +0100

    Added folder structure, scripts and example scene

---
+++ Unity-Array-Modifier/Assets/ArrayModifier/Core/Editor/ArrayModifierEditor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace ArrayModifier
+{
+    [CustomEditor(typeof(ArrayModifier))]
+    public class ArrayModifierEditor : Editor
+    {
+        private ArrayModifier _target;
+
+        private void Awake()
+        {
+            _target = target as ArrayModifier;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+
+            _target._fitType = (FitType)EditorGUILayout.EnumPopup(_target._fitType);
+
+            if (_target._fitType == FitType.FixedCount)
+            {
+                _target._count = EditorGUILayout.DelayedIntField(_target._count);
+            }
+
+            _target._constantOffset = EditorGUILayout.Vector3Field("Constant Offset", _target._constantOffset);
+
+            var valuesHaveBeenChanged = EditorGUI.EndChangeCheck();
+            if (valuesHaveBeenChanged)
+            {
+                UpdateArrayModifier();
+            }
+        }
+
+        private void UpdateArrayModifier()
+        {
+            if (_target._isDuplicate) return;
+
+            _target.RemoveDuplicates();
+            _target.InstantiateDuplicates();
+        }
+    }
+}
---
+++ Unity-Array-Modifier/Assets/ArrayModifier/Core/Runtime/ArrayModifier.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ArrayModifier
+{
+    public class ArrayModifier : MonoBehaviour
+    {
+        [HideInInspector] public FitType _fitType = FitType.FixedCount;
+        [HideInInspector] public int _count = 1;
+        [HideInInspector] public Vector3 _constantOffset = Vector3.one;
+
+        public bool _isDuplicate = false;
+        [HideInInspector] public List<GameObject> _duplicates = new List<GameObject>();
+
+        private void OnDestroy()
+        {
+            RemoveDuplicates();
+        }
+
+        public void RemoveDuplicates()
+        {
+            if (_isDuplicate) return;
+
+            foreach (var duplicate in _duplicates)
+            {
+                DestroyImmediate(duplicate);
+            }
+
+            _duplicates.Clear();
+        }
+
+        public void InstantiateDuplicates()
+        {
+            if (_isDuplicate) return;
+
+            for (int i = 1; i < _count; i++)
+            {
+                var duplicate = Object.Instantiate(gameObject);
+
+                var arrayModifier = duplicate.GetComponent<ArrayModifier>();
+                arrayModifier.RemoveDuplicates();
+                DestroyImmediate(arrayModifier);
+
+                duplicate.transform.position = transform.position + _constantOffset * (float)i;
+                duplicate.hideFlags = HideFlags.HideInHierarchy | HideFlags.NotEditable | HideFlags.HideInInspector;
+                duplicate.transform.SetParent(transform, true);
+                
+                _duplicates.Add(duplicate);
+            }
+        }
+    }
+}
