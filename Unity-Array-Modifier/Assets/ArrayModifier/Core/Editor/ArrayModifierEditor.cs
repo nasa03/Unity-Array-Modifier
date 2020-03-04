a6cd246122251484dc1c2fb8aebd2c5d9be6ac62
Samuel <samuel.karabetian@gmail.com>
2020-03-04 18:01:20 +0100

    Added support for stacking ArrayModifier components

--- Unity-Array-Modifier/Assets/ArrayModifier/Core/Editor/ArrayModifierEditor.cs
+++ Unity-Array-Modifier/Assets/ArrayModifier/Core/Editor/ArrayModifierEditor.cs
@@ -17,28 +17,63 @@ namespace ArrayModifier
         {
             EditorGUI.BeginChangeCheck();
 
-            _target._fitType = (FitType)EditorGUILayout.EnumPopup("Fit Type", _target._fitType);
+            _target._fitType = (FitType)EditorGUILayout.EnumPopup(new GUIContent("Fit Type", "Determines which method to use for creating duplicates."), _target._fitType);
 
             if (_target._fitType == FitType.FixedCount)
             {
-                _target._count = EditorGUILayout.DelayedIntField(_target._count);
+                _target._count = EditorGUILayout.DelayedIntField(new GUIContent("Count", "Determines how many duplicates will get created."), _target._count);
             }
 
-            _target._constantOffset = EditorGUILayout.Vector3Field("Constant Offset", _target._constantOffset);
+            _target._constantOffset = EditorGUILayout.Vector3Field(new GUIContent("Constant Offset", "Determines the offset between each object in global space."), _target._constantOffset);
+            _target._relativeOffset = EditorGUILayout.Vector3Field(new GUIContent("Relative Offset", "Determines the offset between each object in local space."), _target._relativeOffset);
 
             var valuesHaveBeenChanged = EditorGUI.EndChangeCheck();
             if (valuesHaveBeenChanged)
             {
-                UpdateArrayModifier();
+                Calculate();
             }
         }
 
-        private void UpdateArrayModifier()
+        private void Calculate() 
         {
-            if (_target._isDuplicate) return;
+            for (int i = _target.transform.childCount - 1; i >= 0; i--)
+            {
+                DestroyImmediate(_target.transform.GetChild(i).gameObject);
+            }
+
+            var arrayModifiers = _target.GetComponents<ArrayModifier>();
+
+            Transform lastArrayModifierTransform = null;
+
+            foreach (var arrayModifier in arrayModifiers)
+            {
+                arrayModifier.InstantiateDuplicates();
+
+                if (lastArrayModifierTransform != null)
+                {
+                    arrayModifier.transform.SetParent(lastArrayModifierTransform, true);
+                }
+
+                lastArrayModifierTransform = arrayModifier.transform;
+            }
+        }
+
+        private void OnSceneGUI()
+        {
+            if (_target.transform.hasChanged) OnChangedTransform();
+        }
+
+        private void OnChangedTransform()
+        { 
+            Calculate();
+            _target.transform.hasChanged = false;
+        }
+
+        private void OnDestroy()
+        {
+            if (_target != null) return;
 
             _target.RemoveDuplicates();
-            _target.InstantiateDuplicates();
         }
     }
 }
--- Unity-Array-Modifier/Assets/ArrayModifier/Core/Runtime/ArrayModifier.cs
+++ Unity-Array-Modifier/Assets/ArrayModifier/Core/Runtime/ArrayModifier.cs
@@ -7,46 +7,64 @@ namespace ArrayModifier
     {
         [HideInInspector] public FitType _fitType = FitType.FixedCount;
         [HideInInspector] public int _count = 1;
-        [HideInInspector] public Vector3 _constantOffset = Vector3.one;
+        [HideInInspector] public Vector3 _constantOffset = Vector3.right;
+        [HideInInspector] public Vector3 _relativeOffset = Vector3.zero;
 
-        public bool _isDuplicate = false;
-        [HideInInspector] public List<GameObject> _duplicates = new List<GameObject>();
-
-        private void OnDestroy()
-        {
-            RemoveDuplicates();
-        }
+        private List<GameObject> _duplicates = new List<GameObject>();
 
         public void RemoveDuplicates()
         {
-            if (_isDuplicate) return;
+            if (_duplicates == null) return;
 
             foreach (var duplicate in _duplicates)
             {
+                if (duplicate == null) continue;
+
                 DestroyImmediate(duplicate);
             }
 
             _duplicates.Clear();
         }
 
+        public void UpdateArrayModifier()
+        {
+            RemoveDuplicates();
+            InstantiateDuplicates();
+        }
+
         public void InstantiateDuplicates()
         {
-            if (_isDuplicate) return;
+            var prefab = Object.Instantiate(gameObject);
 
             for (int i = 1; i < _count; i++)
             {
-                var duplicate = Object.Instantiate(gameObject);
+                var duplicate = Object.Instantiate(prefab);
 
-                var arrayModifier = duplicate.GetComponent<ArrayModifier>();
-                arrayModifier.RemoveDuplicates();
-                DestroyImmediate(arrayModifier);
+                var constantOffsetVector = _constantOffset * (float)i;
 
-                duplicate.transform.position = transform.position + _constantOffset * (float)i;
+                var relativeOffsetVector = transform.right * _relativeOffset.x + transform.up * _relativeOffset.y + transform.forward * _relativeOffset.z;
+                relativeOffsetVector *= (float)i;
+
+                duplicate.transform.position = transform.position + constantOffsetVector + relativeOffsetVector;
                 duplicate.hideFlags = HideFlags.HideInHierarchy | HideFlags.NotEditable | HideFlags.HideInInspector;
                 duplicate.transform.SetParent(transform, true);
-                
+
+                DestroyImmediate(duplicate.GetComponent<ArrayModifier>());
+
                 _duplicates.Add(duplicate);
             }
+
+            DestroyImmediate(prefab);
+        }
+
+        private void OnDestroy()
+        {
+            RemoveDuplicates();
+        }
+
+        private void OnDisable()
+        {
+            RemoveDuplicates();
         }
     }
 }
