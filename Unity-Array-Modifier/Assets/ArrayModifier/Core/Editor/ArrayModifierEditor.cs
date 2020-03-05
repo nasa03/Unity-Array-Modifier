e6ba18b78a664c63966b4269afbb96dc2b827969
Samuel <samuel.karabetian@gmail.com>
2020-03-05 12:33:55 +0100

    Refactor and bug fixes

--- Unity-Array-Modifier/Assets/ArrayModifier/Core/Editor/ArrayModifierEditor.cs
+++ Unity-Array-Modifier/Assets/ArrayModifier/Core/Editor/ArrayModifierEditor.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using System.Linq;
+using UnityEditor.SceneManagement;
 
 namespace ArrayModifier
 {
@@ -8,6 +8,8 @@ namespace ArrayModifier
     public class ArrayModifierEditor : Editor
     {
         private ArrayModifier _target;
+        private bool _sceneWasDirtyLastTick = false;
+        private bool _targetGOWasDirtyLastTick = false;
 
         private void Awake()
         {
@@ -16,6 +18,15 @@ namespace ArrayModifier
 
         public override void OnInspectorGUI()
         {
+            GUILayout.BeginHorizontal();
+
+            if (GUILayout.Button("Refresh"))
+            {
+                _target.Calculate();
+            }
+
+            GUILayout.EndHorizontal();
+
             EditorGUI.BeginChangeCheck();
 
             _target._fitType = (FitType)EditorGUILayout.EnumPopup(new GUIContent("Fit Type", "Determines which method to use for creating duplicates."), _target._fitType);
@@ -31,54 +42,70 @@ namespace ArrayModifier
             var valuesHaveBeenChanged = EditorGUI.EndChangeCheck();
             if (valuesHaveBeenChanged)
             {
-                Calculate();
-            }
-        }
-
-        private void Calculate() 
-        {
-            for (int i = _target.transform.childCount - 1; i >= 0; i--)
-            {
-                DestroyImmediate(_target.transform.GetChild(i).gameObject);
-            }
-
-            var arrayModifiers = _target.GetComponents<ArrayModifier>();
-
-            Transform lastArrayModifierTransform = null;
-
-            foreach (var arrayModifier in arrayModifiers)
-            {
-                arrayModifier.InstantiateDuplicates();
-
-                if (lastArrayModifierTransform != null)
-                {
-                    arrayModifier.transform.SetParent(lastArrayModifierTransform, true);
-                }
-
-                lastArrayModifierTransform = arrayModifier.transform;
-            }
-
-            foreach (var arrayModifier in arrayModifiers)
-            {
-                var childArrayModifiers = arrayModifier.GetComponentsInChildren<ArrayModifier>()
-                    .Where(a => a.gameObject != _target.gameObject);
-
-                foreach (var childArrayModifier in childArrayModifiers)
-                {
-                    childArrayModifier.gameObject.hideFlags = HideFlags.NotEditable | HideFlags.HideInHierarchy | HideFlags.HideInInspector;
-                    DestroyImmediate(childArrayModifier);
-                }
+                _target.Calculate();
             }
         }
 
         private void OnSceneGUI()
         {
+            if (_target == null) return;
+
+            CheckForSceneChanges();
+            CheckForTargetGameObjectChanges();
+
             if (_target.transform.hasChanged) OnChangedTransform();
+        }
+
+        private void CheckForSceneChanges() 
+        {
+            var activeScene = EditorSceneManager.GetActiveScene();
+            if (activeScene.isDirty && !_sceneWasDirtyLastTick)
+            {
+                OnSceneChanged();
+            }
+
+            _sceneWasDirtyLastTick = activeScene.isDirty;
+        }
+
+        private void CheckForTargetGameObjectChanges() 
+        {
+            if (_target == null) return;
+
+            var targetGOIsDirty = false;
+
+            var components = _target.gameObject.GetComponents<MonoBehaviour>();
+            foreach (var component in components)
+            {
+                if (EditorUtility.IsDirty(component))
+                {
+                    targetGOIsDirty = true;
+                    break;
+                }
+            }
+
+            if (targetGOIsDirty && !_targetGOWasDirtyLastTick)
+            {
+                OnTargetGOChanged();
+            }
+
+            _targetGOWasDirtyLastTick = targetGOIsDirty;
+        }
+
+        private void OnSceneChanged() 
+        {
+            _target.Calculate();
+            SceneView.RepaintAll();
+        }
+
+        private void OnTargetGOChanged()
+        {
+            _target.Calculate();
+            SceneView.RepaintAll();
         }
 
         private void OnChangedTransform()
         { 
-            Calculate();
+            _target.Calculate();
             _target.transform.hasChanged = false;
         }
 
