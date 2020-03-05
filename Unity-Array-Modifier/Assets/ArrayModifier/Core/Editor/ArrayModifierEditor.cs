1fad6c627bf0c0a687f95f9a46df11afebc910fe
Samuel <samuel.karabetian@gmail.com>
2020-03-05 14:24:51 +0100

    Implemented undo/redo operations

--- Unity-Array-Modifier/Assets/ArrayModifier/Core/Editor/ArrayModifierEditor.cs
+++ Unity-Array-Modifier/Assets/ArrayModifier/Core/Editor/ArrayModifierEditor.cs
@@ -16,32 +16,62 @@ namespace ArrayModifier
             _target = target as ArrayModifier;
         }
 
+        private void OnEnable()
+        {
+            Undo.undoRedoPerformed += _target.Calculate;
+        }
+
+        private void OnDisable()
+        {
+            Undo.undoRedoPerformed -= _target.Calculate;
+        }
+
         public override void OnInspectorGUI()
         {
             GUILayout.BeginHorizontal();
-
-            if (GUILayout.Button("Refresh"))
-            {
-                _target.Calculate();
-            }
-
+                if (GUILayout.Button("Refresh"))
+                {
+                    _target.Calculate();
+                }
             GUILayout.EndHorizontal();
 
             EditorGUI.BeginChangeCheck();
+                var fitType = (FitType)EditorGUILayout.EnumPopup(new GUIContent("Fit Type", "Determines which method to use for creating duplicates."), _target._fitType);
 
-            _target._fitType = (FitType)EditorGUILayout.EnumPopup(new GUIContent("Fit Type", "Determines which method to use for creating duplicates."), _target._fitType);
+                var count = _target._count;
+                if (_target._fitType == FitType.FixedCount)
+                {
+                    count = EditorGUILayout.DelayedIntField(new GUIContent("Count", "Determines how many duplicates will get created."), _target._count);
+                }
 
-            if (_target._fitType == FitType.FixedCount)
+                var constantOffset = EditorGUILayout.Vector3Field(new GUIContent("Constant Offset", "Determines the offset between each object in global space."), _target._constantOffset);
+                var relativeOffset = EditorGUILayout.Vector3Field(new GUIContent("Relative Offset", "Determines the offset between each object in local space."), _target._relativeOffset);
+            if (EditorGUI.EndChangeCheck())
             {
-                _target._count = EditorGUILayout.DelayedIntField(new GUIContent("Count", "Determines how many duplicates will get created."), _target._count);
-            }
+                if (fitType != _target._fitType)
+                {
+                    Undo.RecordObject(_target, "Changed fit type");
+                    _target._fitType = fitType;
+                }
 
-            _target._constantOffset = EditorGUILayout.Vector3Field(new GUIContent("Constant Offset", "Determines the offset between each object in global space."), _target._constantOffset);
-            _target._relativeOffset = EditorGUILayout.Vector3Field(new GUIContent("Relative Offset", "Determines the offset between each object in local space."), _target._relativeOffset);
+                if (count != _target._count)
+                {
+                    Undo.RecordObject(_target, "Changed count");
+                    _target._count = count;
+                }
 
-            var valuesHaveBeenChanged = EditorGUI.EndChangeCheck();
-            if (valuesHaveBeenChanged)
-            {
+                if (constantOffset != _target._constantOffset)
+                {
+                    Undo.RecordObject(_target, "Changed constant offset");
+                    _target._constantOffset = constantOffset;
+                }
+
+                if (relativeOffset != _target._relativeOffset)
+                {
+                    Undo.RecordObject(_target, "Changed relative offset");
+                    _target._relativeOffset = relativeOffset;
+                }
+
                 _target.Calculate();
             }
         }
